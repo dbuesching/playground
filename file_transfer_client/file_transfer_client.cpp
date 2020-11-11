4059a81179338f570f1b6c6f46e1f6fa698bcc3e
Dietrich <dietrich@dietrich-Aspire-XC-886>
2020-11-11 14:35:42 +0100

    reformat

--- file_transfer_client/file_transfer_client.cpp
+++ file_transfer_client/file_transfer_client.cpp
@@ -25,57 +25,57 @@ std::ifstream inStream;
 
 
 void write_handler(const boost::system::error_code &ec,
-	std::size_t bytes_transferred)
+                   std::size_t bytes_transferred)
 {
-	if (!ec)
-	{
-		std::cout << "written a block" << std::endl;
-		inStream.read(bytes.data(), bytes.size());
-		auto sizeRead = inStream.gcount();
-		if (sizeRead > 0)
-			tcp_socket.async_write_some(buffer(bytes, sizeRead), write_handler);
+    if (!ec)
+    {
+        std::cout << "written a block" << std::endl;
+        inStream.read(bytes.data(), bytes.size());
+        auto sizeRead = inStream.gcount();
+        if (sizeRead > 0)
+            tcp_socket.async_write_some(buffer(bytes, sizeRead), write_handler);
         else
             inStream.close();
-	}
-	else
-	{
-		std::cout << "no more data to read\n" << std::endl;
+    }
+    else
+    {
+        std::cout << "no more data to read\n" << std::endl;
         inStream.close();
-	}
+    }
 }
 
 void connect_handler(const boost::system::error_code &ec)
 {
-	if (!ec)
-	{
-		inStream.read(bytes.data(), bytes.size());
-		auto sizeRead = inStream.gcount();
-		if (sizeRead > 0)
-			tcp_socket.async_write_some(buffer(bytes, sizeRead), write_handler);
-	}
+    if (!ec)
+    {
+        inStream.read(bytes.data(), bytes.size());
+        auto sizeRead = inStream.gcount();
+        if (sizeRead > 0)
+            tcp_socket.async_write_some(buffer(bytes, sizeRead), write_handler);
+    }
 }
 
 void resolve_handler(const boost::system::error_code &ec,
-	tcp::resolver::iterator it)
+                     tcp::resolver::iterator it)
 {
-	if (!ec)
-		tcp_socket.async_connect(*it, connect_handler);
+    if (!ec)
+        tcp_socket.async_connect(*it, connect_handler);
 }
 
 void test_file_transfer_client(const std::string& ip, const std::string& sFilename )
 {
-	using namespace std;
-	inStream.open(sFilename.c_str(), ios_base::binary | ios_base::in);
-	tcp::resolver::query q{ ip, "2014" };
-	resolv.async_resolve(q, resolve_handler);
-	ioservice.run();
+    using namespace std;
+    inStream.open(sFilename.c_str(), ios_base::binary | ios_base::in);
+    tcp::resolver::query q{ ip, "2014" };
+    resolv.async_resolve(q, resolve_handler);
+    ioservice.run();
 }
 
 
 int main(int argc, char** argv)
 {
-	std::string ip = argv[1];
-	std::string sFilename = argv[2];
-	test_file_transfer_client(ip, sFilename);
+    std::string ip = argv[1];
+    std::string sFilename = argv[2];
+    test_file_transfer_client(ip, sFilename);
 }
 

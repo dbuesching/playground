b036e7a6a74f8ff595a1d6b713a65a91dd04d790
dietrich <dbuesching@web.de>
2020-11-11 09:16:40 +0100

    factor out url to static variable

--- schaeling_examples/schaeling_examples.cpp
+++ schaeling_examples/schaeling_examples.cpp
@@ -93,6 +93,9 @@ tcp::resolver resolv{ ioservice };
 tcp::socket tcp_socket{ ioservice };
 std::array<char, 4096> bytes;
 
+//std::string url = "theboostcpplibraries.com";
+std::string url = "www.heise.de";
+
 void read_handler(const boost::system::error_code &ec,
 	std::size_t bytes_transferred)
 {
@@ -101,6 +104,10 @@ void read_handler(const boost::system::error_code &ec,
 		std::cout.write(bytes.data(), bytes_transferred);
 		tcp_socket.async_read_some(buffer(bytes), read_handler);
 	}
+	else
+	{
+		std::cout << "no more data to read\n" << std::endl;
+	}
 }
 
 void connect_handler(const boost::system::error_code &ec)
@@ -108,7 +115,8 @@ void connect_handler(const boost::system::error_code &ec)
 	if (!ec)
 	{
 		std::string r =
-			"GET / HTTP/1.1\r\nHost: theboostcpplibraries.com\r\n\r\n";
+			"GET / HTTP/1.1\r\nHost: ";
+			r += url; r+="\r\n\r\n";
 		write(tcp_socket, buffer(r));
 		tcp_socket.async_read_some(buffer(bytes), read_handler);
 	}
@@ -123,7 +131,7 @@ void resolve_handler(const boost::system::error_code &ec,
 
 void testWebClient()
 {
-	tcp::resolver::query q{ "theboostcpplibraries.com", "80" };
+	tcp::resolver::query q{ url, "80" };
 	resolv.async_resolve(q, resolve_handler);
 	ioservice.run();
 }
